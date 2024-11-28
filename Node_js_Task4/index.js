adba668571bff948fce7ae14c99c7b54ccb33e44
Nikolay <meta1mang@mail.ru>
2024-11-28 19:13:34 +0300

    Task4_removal method

--- Node_js_Task4/index.js
+++ Node_js_Task4/index.js
@@ -1,18 +1,44 @@
 const express = require("express");
+const joi = require("joi");
 
 const app = express();
 
-let uniqueID = 0;
 const users = [];
+let uniqueID = 0;
+
+/**
+ * Описание объекта пользователя, для проверки валидности
+ */
+const userSchema = joi.object({
+    firstName: joi.string().min(2).required(),
+    secondName: joi.string().min(2).required(),
+    age: joi.number().min(0).max(150).required(),
+    city: joi.string().min(1) 
+});
 
 app.use(express.json());
 
 /**
  * Получение списка пользователей
  */
-app.get("/users", (reg, res) => {
+app.get("/users", (req, res) => {
   res.send({ users });
 });
+
+/**
+ * Получение отдельного пользователя по id
+ */
+app.get('/users/:id', (req, res) => {
+    const user = users.find((user) => user.id === Number(req.params.id));
+    // проверка наличия пользователя
+    if (user) {
+      res.send({user});
+    } else {
+      res.status(404);
+      res.send({user: null})
+    }
+  });
+
 
 /**
  * Создание пользователя
@@ -33,6 +59,11 @@ app.post("/users", (req, res) => {
  * Метод внесения изменении в данные о пользователе, по id
  */
 app.put('/users/:id', (req, res) => {
+    const result = userSchema.validate(req.body);
+    if (result.error){
+      return res.status(404).send({ error: result.error.details });
+    }
+
     const user = users.find((user) => user.id === Number(req.params.id));
   
     // проверка наличия данных
@@ -50,6 +81,21 @@ app.put('/users/:id', (req, res) => {
     }
 });
 
-
+/**
+ * Метод удаления пользователя
+ */
+app.delete('/users/:id', (req, res) => {
+    const user = users.find((user) => user.id === Number(req.params.id));
+  
+    // проверка наличия и удаление пользователя по id
+    if (user) {
+      const userIndex = users.indexOf(user);
+      users.splice(userIndex, 1);
+      res.send({user});
+    } else {
+      res.status(404);
+      res.send({user: null})
+    }
+  });
 
 app.listen(3000);
