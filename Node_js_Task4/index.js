8930239b0239a614f74a6eccf14556073e0abf2d
Nikolay <meta1mang@mail.ru>
2024-11-28 15:09:08 +0300

    Task4_Create a user

--- Node_js_Task4/index.js
+++ Node_js_Task4/index.js
@@ -1,16 +1,32 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
+let uniqueID = 0;
 const users = [];
 
-let uniqueID = 0;
+app.use(express.json());
 
 /**
- * Получение списка пользователей 
+ * Получение списка пользователей
  */
-app.get('/users', (reg, res) => {
-  res.send({users});
+app.get("/users", (reg, res) => {
+  res.send({ users });
+});
+
+/**
+ * Создание пользователя
+ */
+app.post("/users", (req, res) => {
+  uniqueID += 1;
+  users.push({
+    id: uniqueID,
+    ...req.body, // сюда запишется объект, который пропишем в POSTMAN
+  });
+
+  res.send({
+    id: uniqueID,
+  });
 });
 
 app.listen(3000);
